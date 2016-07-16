25c83bbca4b98ba4008ca1b354235432f2fc04e6
grimal <phillip@philliprstafford.com>
2016-07-16 09:25:04 -0400

    Change AnimatedOpacity to cycle each tile in succession.  Check if this removes the intermittent pause effect generated by the random number.  Suspect the random number is generating the same number and this is causing the hangup.

--- js/AnimatedOpacity.js
+++ js/AnimatedOpacity.js
@@ -1,54 +1,58 @@
-$(function() {
 
-    function startFadeIn() {
-        // if ($("#company").offset().top < 150) {
-        if (window.pageYOffset > 200) {
-            console.log('start fading');
-            $('#tile-art').addClass('tile-show');
-
-            // }
-        }
-    }
-
-
-    // $( "#company" ).scroll(function() {
-    //     startFadeIn();
-    // });
-    // var shit = $('#company');
-    // console.log(shit);
-    // shit.addEventListener("scroll", startFadeIn);
-    window.addEventListener("scroll", startFadeIn);
-
-    var tiles = $('.tile').get();
-    // console.log(tiles);
-    function tileFadein(e) {
-
-        var tile = tiles.splice(Math.floor(Math.random() * tiles.length), 1);
-
-
-        $(tile).addClass('tile-show');
-
-        console.log('done fading');
-        // console.log(window.pageYOffset);
+function startFadeIn() {
+    // if ($("#company").offset().top < 150) {
+    if (window.pageYOffset > 375) {
+        console.log('start fading');
+        $('#tile-art').addClass('tile-show');
 
     }
+}
+
+var x = 1;
+var tiles = $('.tile').get();
+function addFadeListeners() {
+    //var tiles = $('.tile').get();
     for (var idx = 0; idx < tiles.length; idx++) {
-        // console.log($("#company").offset().top);
-        // console.log(window.pageYOffset);
-        // console.log(tiles[idx]);
         tiles[idx].addEventListener("transitionend", tileFadein, false);
         tiles[idx].addEventListener("webkitTransitionEnd", tileFadein, false);
         tiles[idx].addEventListener("mozTransitionEnd", tileFadein, false);
         tiles[idx].addEventListener("msTransitionEnd", tileFadein, false);
         tiles[idx].addEventListener("oTransitionEnd", tileFadein, false);
+        console.log('added listener.');
+        console.log(tiles[idx]);
+
+    }
+}
+
+
+$(window).scroll(startFadeIn);
+$(document).ready(startFadeIn);
+$(document).ready(addFadeListeners);
+
+
+
+    // window.addEventListener("scroll", startFadeIn);
+    //
+    // var tiles = $('.tile').get();
+    // // console.log(tiles);
+    function tileFadein(e) {
+
+        // var tile = tiles.splice(Math.floor(Math.random() * tiles.length), 1);
+        var tile = tiles[x];
+
+        $(tile).addClass('tile-show');
+        console.log(tiles);
+        console.log('done fading');
+        console.log(tile);
+        x++;
+
 
     }
 
 
 
-    console.log($("#company").offset().top );
 
-});
+
 
 function isScrolledIntoView(elem)
 {
@@ -61,6 +65,6 @@ function isScrolledIntoView(elem)
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
 
-// $(document).ready(startFadeIn);
+
 
 
