d844a470775c28cda57c29b7c6b2b1ede4519b63
grimal <phillip@philliprstafford.com>
2016-09-18 16:25:05 -0400

    Clean up CSS entirely for all MQ and all layouts.

--- dev/js/app.js
+++ dev/js/app.js
@@ -6,16 +6,6 @@ var fade = false;
 var r;
 var c = 0;
 
-
-// Prevent over-scrolling on iOS devices.
-
-// disabled due to touch scrolling no longer working.
-
-// document.body.addEventListener('touchmove', function(event) {
-//     event.preventDefault();
-// }, false);
-
-
 $(function() {
     new WOW().init();
     collapseNavbar();
@@ -84,146 +74,28 @@ $('.navbar-collapse ul li a').click(function() {
 });
 
 
-// Google Maps Scripts
-// var map = null;
-// // When the window has finished loading create our google map below
-// google.maps.event.addDomListener(window, 'load', init);
-// google.maps.event.addDomListener(window, 'resize', function() {
-//     map.setCenter(new google.maps.LatLng(40.757118, -73.971890));
-// });
-
 function initMap() {
-    console.log('here in initmap.');
+    // console.log('here in initmap.');
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 16,
 
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(40.757118, -73.971890), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
-        scrollwheel: false,
+        scrollwheel: true,
         draggable: false,
 
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
-        // styles: [{
-        //     "featureType": "water",
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 17
-        //     }]
-        // }, {
-        //     "featureType": "landscape",
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 20
-        //     }]
-        // }, {
-        //     "featureType": "road.highway",
-        //     "elementType": "geometry.fill",
-        //     "stylers": [{
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 17
-        //     }]
-        // }, {
-        //     "featureType": "road.highway",
-        //     "elementType": "geometry.stroke",
-        //     "stylers": [{
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 29
-        //     }, {
-        //         "weight": 0.2
-        //     }]
-        // }, {
-        //     "featureType": "road.arterial",
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 18
-        //     }]
-        // }, {
-        //     "featureType": "road.local",
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 16
-        //     }]
-        // }, {
-        //     "featureType": "poi",
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 21
-        //     }]
-        // }, {
-        //     "elementType": "labels.text.stroke",
-        //     "stylers": [{
-        //         "visibility": "on"
-        //     }, {
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 16
-        //     }]
-        // }, {
-        //     "elementType": "labels.text.fill",
-        //     "stylers": [{
-        //         "saturation": 36
-        //     }, {
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 40
-        //     }]
-        // }, {
-        //     "elementType": "labels.icon",
-        //     "stylers": [{
-        //         "visibility": "off"
-        //     }]
-        // }, {
-        //     "featureType": "transit",
-        //     "elementType": "geometry",
-        //     "stylers": [{
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 19
-        //     }]
-        // }, {
-        //     "featureType": "administrative",
-        //     "elementType": "geometry.fill",
-        //     "stylers": [{
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 20
-        //     }]
-        // }, {
-        //     "featureType": "administrative",
-        //     "elementType": "geometry.stroke",
-        //     "stylers": [{
-        //         "color": "#000000"
-        //     }, {
-        //         "lightness": 17
-        //     }, {
-        //         "weight": 1.2
-        //     }]
-        // }]
     };
 
     // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
-    console.log(mapElement);
+    // console.log(mapElement);
     // Create the Google Map using out element and options defined above
     map = new google.maps.Map(mapElement, mapOptions);
 
@@ -235,6 +107,11 @@ function initMap() {
         map: map
         // icon: image
     });
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+    google.maps.event.addDomListener(window, 'resize', function() {
+    map.setCenter(new google.maps.LatLng(40.757118, -73.971890));
+        });
 }
 
 
@@ -254,7 +131,7 @@ function addFadeListeners() {
 
     // Initiate Fade on first wheel
     var $elem = $('#constellation-one');
-    console.log('Adding show to first wheel');
+    // console.log('Adding show to first wheel');
     $elem.addClass('wheel-show');
 }
 
@@ -281,15 +158,13 @@ function wheelFadein(e) {
 }
 
 function addTouchEvents() {
-    console.log('adding touch event');
+    // console.log('adding touch event');
 
     var $elem = document.getElementById('contact-email');
 
     $elem.addEventListener('touchstart', function(e) {
         e.preventDefault();
-        // alert(e.changedTouches[0].pageX) // alert pageX coordinate of touch point
-        console.log('touch detected');
-        window.open('mailto:test@example.com?subject=subject&body=body');
+        window.open('mailto:info@corrcapital.com?subject=Information Requested');
     }, false);
 
 
