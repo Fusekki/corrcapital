1b074c520a6200bc8b67d84504e1a092274f0d1c
grimal <phillip@philliprstafford.com>
2016-09-20 21:06:01 -0400

    Correct include directory for phpmailer.  Incorporate script suggested by hosting tech.

---
+++ dev/phpmailertesting/test1.php
@@ -0,0 +1,34 @@
+<?php
+require 'PHPMailer-master/PHPMailerAutoload.php';
+$mail = new PHPMailer();
+
+
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+//From email address and name
+$mail->From = "donotreply@corrcapital.com";
+$mail->FromName = "Corr Capital Staff";
+
+
+//To address and name
+$mail->addAddress("phillip@philliprstafford.com", "Recepient Name");
+//Address to which recipient will reply
+$mail->addReplyTo("phptester@corrcapital.philliprstafford.com", "Reply");
+
+//CC and BCC
+
+//Send HTML or Plain Text email
+$mail->isHTML(true);
+
+$mail->Subject = "Responding to you.";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+
+if(!$mail->send())
+{
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
+else
+{
+    echo "Message has been sent successfully";
+}
